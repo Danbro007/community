4ccd5f03493216c51dd9141bcb29cf164f5970a2
danbro <710170342@qq.com>
2019-08-02 19:03:25 +0800

    添加github账号授权功能

---
+++ src/main/java/lfie/danbro/community/community/controller/AuthorizeController.java
@@ -0,0 +1,35 @@
+package lfie.danbro.community.community.controller;
+
+
+import lfie.danbro.community.community.dto.AccessTokenDto;
+import lfie.danbro.community.community.dto.GitHubUser;
+import lfie.danbro.community.community.provider.GitHubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    GitHubProvider gitHubProvider;
+
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam("code") String code,
+                           @RequestParam("state") String state) {
+        AccessTokenDto accessTokenDto = new AccessTokenDto();
+        accessTokenDto.setClient_id("06fdb966a8cbcf08c463");
+        accessTokenDto.setClient_secret("a5d60bc8db4b9665ec82e31a04c809e57d4e4ea3");
+        accessTokenDto.setCode(code);
+        accessTokenDto.setRedirect_uri("http://localhost:8888/callback");
+        accessTokenDto.setState(state);
+        String accessToken = gitHubProvider.getAccessToken(accessTokenDto);
+        System.out.println(accessToken);
+        GitHubUser user = gitHubProvider.getGitUser(accessToken);
+        System.out.println(user);
+        return "index";
+    }
+
+}
---
+++ src/main/java/lfie/danbro/community/community/provider/GitHubProvider.java
@@ -0,0 +1,49 @@
+package lfie.danbro.community.community.provider;
+
+
+import com.alibaba.fastjson.JSON;
+import lfie.danbro.community.community.dto.AccessTokenDto;
+import lfie.danbro.community.community.dto.GitHubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class GitHubProvider {
+
+    public String getAccessToken(AccessTokenDto accessTokenDto){
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDto));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            return string.split("&")[0].split("=")[1];
+        } catch (IOException e) {
+        }
+        return null;
+    }
+
+    public GitHubUser getGitUser(String accessToken){
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token=" + accessToken)
+                .get()//默认就是GET请求，可以不写
+                .build();
+        try {
+            Response response = okHttpClient.newCall(request).execute();
+            String string = response.body().string();
+            System.out.println(string);
+            GitHubUser gitHubUser = JSON.parseObject(string,GitHubUser.class);
+            return gitHubUser;
+        } catch (IOException e) {
+        }
+        return null;
+    }
+
+
+}
